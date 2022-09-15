bdbfb1c09262759ab3ee20f02b26fb4456767822
Himanshu Jain <himajai2@cisco.com>
2022-09-15 17:41:58 +0530

    update

---
+++ Algorithms/Patterns/d.java
@@ -0,0 +1,33 @@
+package Algorithms.Patterns;
+
+/*
+
+1
+22
+333
+4444
+55555
+
+*/
+
+public class d {
+    public static void main(String[] args) {
+        
+
+        for(int i=0; i<5; i++) {
+            int j=0;
+            while(j<=i) {
+                System.out.print(i+1);
+                j++;
+            }
+
+            while(j<5) {
+                System.out.print(" ");
+                j++;
+            }
+            System.out.println();
+        }
+
+
+    }
+}
