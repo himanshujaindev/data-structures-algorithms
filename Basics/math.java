bbe37652d4c7264da2999fbd538039689622ba57
Himanshu Jain <himajai2@cisco.com>
2022-09-23 14:23:07 +0530

    update

---
+++ Basics/math.java
@@ -0,0 +1,14 @@
+public class math {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 2;
+
+        System.out.println("Max = " + Math.max(a, b));
+        System.out.println("Min = " + Math.min(a, b));
+
+        System.out.println("Power = " + Math.pow(a, b));
+        System.out.println("Sqrt = " + Math.sqrt(a));
+        System.out.println("Random = " + Math.random()); // Random double
+
+    }
+}
--- QuickCode/leetcode.java
+++ QuickCode/leetcode.java
@@ -1,42 +1,21 @@
 package QuickCode;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-
 public class leetcode {
 
-    public static int findLength(int[] nums1, int[] nums2) {
-        // Same as Longest Commen Substring
-
-        // Compare all prefix of str1 with all prefix of str2 => Store common suffix
-        // => Longest common suffix => LCS
-
-        // dp(i)(j) = dp(i-1)(j-1) + 1 ... if str1[i] = str2[j]
-        // dp(i)(j) = 0 ... if str1[i] != str2[j]
-
-        int[][] dp = new int[nums1.length + 1][nums2.length + 1];
-        int res = 0;
-        for (int i = 1; i <= nums1.length; i++) {
-            for (int j = 1; j <= nums2.length; j++) {
-                if (nums1[i - 1] == nums2[j - 1]) {
-                    dp[i][j] = dp[i - 1][j - 1] + 1;
-                }
-
-                if (dp[i][j] > res) {
-                    res = dp[i][j];
-                }
-            }
+    public static int concatenatedBinary(int n) {
+        long res = 0;
+        long MOD = (long) Math.pow(10, 9) + 7;
+        for (int i = 1; i <= n; i++) {
+            int no_digits = Integer.toBinaryString(i).length();
+            res = (long) (res * Math.pow(2, no_digits) + i) % MOD;
         }
 
-        return res;
+        return (int) res;
     }
 
     public static void main(String[] args) {
 
-        int[] arr1 = { 1, 2, 3, 2, 1 };
-        int[] arr2 = { 3, 2, 1, 4, 7 };
-        int res = findLength(arr1, arr2);
+        int res = concatenatedBinary(12);
         System.out.println(res);
     }
 }
