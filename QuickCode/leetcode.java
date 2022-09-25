07fe7448349b8c2c858e58a438cccf6e2bbaef40
nhimanshujain <nhimanshujain@gmail.com>
2022-09-25 17:18:04 +0530

    update

--- QuickCode/leetcode.java
+++ QuickCode/leetcode.java
@@ -1,5 +1,66 @@
 package QuickCode;
 
+import java.util.ArrayList;
+import java.util.List;
+
+class MyCircularQueue {
+    int[] queue;
+    int start = 0, end = -1, size = 0; // and points to last inserted element
+    int maxCapacity;
+
+    public MyCircularQueue(int k) {
+        queue = new int[k];
+        maxCapacity = k;
+        return;
+    }
+    
+    public boolean enQueue(int value) {
+        if(isFull()) {
+            return false;
+        }
+
+        size++;
+        end = (end + 1) % maxCapacity;
+        queue[end] = value;
+        
+        return true;
+    }
+    
+    public boolean deQueue() {
+        if(isEmpty()) {
+            return false;
+        }
+
+        size--;
+        start = (start + 1) % maxCapacity;
+        return true;
+    }
+    
+    public int Front() {
+        if(isEmpty()) {
+            return -1;
+        }
+
+        return queue[start];
+    }
+    
+    public int Rear() {
+        if(isEmpty()) {
+            return -1;
+        }
+
+        return queue[end];
+    }
+    
+    public boolean isEmpty() {
+        return size==0;
+    }
+    
+    public boolean isFull() {
+        return size==maxCapacity;
+    }
+}
+
 public class leetcode {
 
     public static int concatenatedBinary(int n) {
