34a862789f1a6510520493857a89c568559d59d8
Himanshu Jain <himajai2@cisco.com>
2022-11-02 12:44:19 +0530

    update

--- DataStructures/Implementation/Graphs/bfs.java
+++ DataStructures/Implementation/Graphs/bfs.java
@@ -19,10 +19,9 @@ public class bfs {
 
     public static void main(String[] args) {
         int[][] graph = new int[][] {
-                { 1, 0, 0, 0 },
-                { 1, 1, 1, 1 },
-                { 0, 1, 0, 1 },
-                { 0, 1, 1, 1 }
+                { 1, 1, 1 },
+                { 1, 1, 1 },
+                { 1, 1, 1 }
         };
 
         int n = graph.length;
