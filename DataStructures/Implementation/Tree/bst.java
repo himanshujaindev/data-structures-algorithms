f2f402990ab68cfe4eb369eb9368a2325cbdb90d
Himanshu Jain <himajai2@cisco.com>
2022-12-07 11:10:29 +0530

    update

---
+++ DataStructures/Implementation/Tree/bst.java
@@ -0,0 +1,8 @@
+package DataStructures.Implementation.Tree;
+
+public class bst {
+    public static void main(String[] args) {
+        // BST Traversal
+        // Sum of elements in BST
+    }
+}
